fc7ac86a51df0a5ef1b53a2438b922040eb8981b
Berra-2000 <2415502023@ogr.mehmetakif.edu.tr>
2025-03-31 20:59:27 +0300

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+K
+Java_2numbersSum_method.iml,9\f\9fc7b14c6b2ae983e755ba1fd4b83326cb178d4c
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = addTwoNumbers(num1, num2);
+        System.out.println("Toplam: " + sum);
+    }
+
+    public static int addTwoNumbers(int a, int b) {
+        return a + b;
+    }
+
+}
